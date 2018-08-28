3811b4b3cba04d4e508c507e6460ab301222d752
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-08-28 13:47:40 +0200

    Update Udp.c

--- Udp.c
+++ Udp.c
@@ -46,8 +46,8 @@ void broke(int s) {
 	// do nothing
 }
 
-#define CONNECTIONS 50000
-#define THREADS 500
+#define CONNECTIONS 8
+#define THREADS 48
 int i;
 void attack(char *host, char *port, int id) {
 	int sockets[CONNECTIONS];
