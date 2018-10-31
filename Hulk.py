419eb7e9a743aae94821edc8ab78549b4a7f9074
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-11-01 00:18:26 +0100

    Update Hulk.py

--- Hulk.py
+++ Hulk.py
@@ -85,7 +85,7 @@ def httpcall(url):
 	request.add_header('Accept', 'text/plain')
 	request.add_header('Host', url)
         request.get_method = lambda: "POST"
-        proxy_support = urllib2.ProxyHandler({"http": "http://www.google.com"})
+        proxy_support = urllib2.ProxyHandler({"https": "https://www.google.com"})
         opener = urllib2.build_opener(proxy_support)
         urllib2.install_opener(opener)
 	
